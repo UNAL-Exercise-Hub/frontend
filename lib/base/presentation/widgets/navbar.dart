4cae5717436e7f1e410f1c6de2030124691ad883
car1803 <55598627+car1803@users.noreply.github.com>
2023-10-19 14:08:38 -0500

    modificar navbar y fotter

--- lib/base/presentation/widgets/navbar.dart
+++ lib/base/presentation/widgets/navbar.dart
@@ -8,7 +8,7 @@ class Navbar extends StatefulWidget implements PreferredSizeWidget {
 
 class _NavbarState extends State<Navbar> {
   Map<String, Color> itemColors = {
-    'Registro': Color(0xFF1D1818),
+    'Detalles': Color(0xFF1D1818),
     'Rutinas': Color(0xFF1D1818),
     'Perfil': Color(0xFF1D1818),
     'Ejercicios': Color(0xFF1D1818),
@@ -17,19 +17,86 @@ class _NavbarState extends State<Navbar> {
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      backgroundColor: Colors.white,
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          _buildNavItem('Registro', Icons.assignment, '/registro'),
-          _buildNavItem('Rutinas', Icons.fitness_center, '/rutinas'),
-          CircleAvatar(
-            radius: 25,
-            backgroundImage: AssetImage('assets/logo.jpg'),
+    bool isMobile = MediaQuery.of(context).size.width < 600;
+
+    if (isMobile) {
+      return AppBar(
+        backgroundColor: Colors.white,
+        title: Row(
+          children: [
+            IconButton(
+              icon: Icon(
+                Icons.menu,
+                color: hoverColor,
+              ),
+              onPressed: () {
+                final RenderBox renderBox =
+                    context.findRenderObject() as RenderBox;
+                final position = renderBox.localToGlobal(Offset.zero);
+
+                showMenu(
+                  context: context,
+                  position: RelativeRect.fromLTRB(
+                    position.dx,
+                    position.dy + kToolbarHeight,
+                    position.dx + renderBox.size.width,
+                    position.dy + kToolbarHeight + renderBox.size.height,
+                  ),
+                  items: [
+                    _buildMobilePopupMenuItem(
+                        'Ejercicios', Icons.assignment, '/ejercicios'),
+                    _buildMobilePopupMenuItem(
+                        'Rutinas', Icons.fitness_center, '/rutinas'),
+                    _buildMobilePopupMenuItem(
+                        'Perfil', Icons.person, '/perfil'),
+                    _buildMobilePopupMenuItem(
+                        'Detalles', Icons.auto_stories, '/detalles'),
+                  ],
+                );
+              },
+            ),
+            Spacer(),
+            CircleAvatar(
+              radius: 25,
+              backgroundImage: AssetImage('assets/logo.jpg'),
+            ),
+          ],
+        ),
+      );
+    } else {
+      return AppBar(
+        backgroundColor: Colors.white,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            _buildNavItem('Ejercicios', Icons.assignment, '/ejercicios'),
+            _buildNavItem('Rutinas', Icons.fitness_center, '/rutinas'),
+            CircleAvatar(
+              radius: 25,
+              backgroundImage: AssetImage('assets/logo.jpg'),
+            ),
+            _buildNavItem('Perfil', Icons.person, '/perfil'),
+            _buildNavItem('Detalles', Icons.auto_stories, '/detalles'),
+          ],
+        ),
+      );
+    }
+  }
+
+  PopupMenuItem<String> _buildMobilePopupMenuItem(
+      String label, IconData icon, String route) {
+    return PopupMenuItem<String>(
+      value: route,
+      child: Row(
+        children: [
+          Icon(icon, color: hoverColor),
+          SizedBox(width: 8),
+          Text(
+            label,
+            style: TextStyle(
+              color: hoverColor,
+            ),
           ),
-          _buildNavItem('Perfil', Icons.person, '/perfil'),
-          _buildNavItem('Ejercicios', Icons.fitness_center, '/ejercicios'),
         ],
       ),
     );
@@ -38,18 +105,15 @@ class _NavbarState extends State<Navbar> {
   Widget _buildNavItem(String label, IconData icon, String route) {
     return InkWell(
       onTap: () {
-        // Navegar a la vista correspondiente
         Navigator.pushNamed(context, route);
       },
       child: MouseRegion(
         onEnter: (_) {
-          // Cambiar el color del texto e icono al pasar el cursor por encima
           setState(() {
             itemColors[label] = hoverColor;
           });
         },
         onExit: (_) {
-          // Restablecer el color del texto e icono cuando el cursor se retira
           setState(() {
             itemColors[label] = Color(0xFF1D1818);
           });
